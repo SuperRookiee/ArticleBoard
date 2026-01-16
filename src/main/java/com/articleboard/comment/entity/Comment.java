e6775199a7d4e670d16aa2fc192ec73b9d6553f1
kystic125 <kystic125@naver.com>
2026-01-16 18:46:00 +0900

    fix: Comment 엔티티 수정

--- src/main/java/com/articleboard/comment/entity/Comment.java
+++ src/main/java/com/articleboard/comment/entity/Comment.java
@@ -1,12 +1,13 @@
-package com.articleboard.comment;
+package com.articleboard.comment.entity;
 
-import com.articleboard.article.Article;
-import com.articleboard.user.User;
+import com.articleboard.article.entity.Article;
+import com.articleboard.user.entity.User;
 import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Table(name = "comment")
 public class Comment {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,11 +24,11 @@ public class Comment {
     private Boolean isDeleted;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "userNo")
+    @JoinColumn(name = "user_no")
     private User user;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "articleNo")
+    @JoinColumn(name = "article_no")
     private Article article;
 
 
