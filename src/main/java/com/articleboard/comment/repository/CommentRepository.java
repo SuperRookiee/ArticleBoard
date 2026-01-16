73625c8bfefd5b5d888fe6f1b4d3d586345a6b3e
kystic125 <kystic125@naver.com>
2026-01-16 18:49:34 +0900

    feat: CommentRepository 추가

---
+++ src/main/java/com/articleboard/comment/repository/CommentRepository.java
@@ -0,0 +1,15 @@
+package com.articleboard.comment.repository;
+
+import com.articleboard.comment.entity.Comment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    Page<Comment> findByArticle_ArticleNo(Long articleNo, Pageable pageable);
+    Page<Comment> findByUser_UserNo(Long userNo, Pageable pageable);
+
+}
